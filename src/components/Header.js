2118d0886f348e59ec61d85153d2426d0bb7b71f
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-06-18 09:44:11 +0900

    import Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../assets/Header.css';
 
 export default class Header extends React.Component {
   render(){
     return (
-      <div>
-
+      <div className="header">
+        
       </div>
     );
   }

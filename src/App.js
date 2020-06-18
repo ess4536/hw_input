798a774541c82d57b58d3e21e57d058707562211
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-06-18 09:36:15 +0900

    add Header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 import MyForm from './components/MyForm';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+        <Header/>
         <MyForm/>
       </div>
     );

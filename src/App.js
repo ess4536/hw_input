e45fe349a2bfcc4d3d3019d9aa502c68f1067fb4
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-06-18 09:48:00 +0900

    add Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MyForm from './components/MyForm';
+import Footer from './components/Footer';
 
 class App extends Component {
   render(){
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <MyForm/>
+        <Footer/>
       </div>
     );
   }
